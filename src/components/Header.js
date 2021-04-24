9fe87f67387238e2b2a0a1c379a10e2871eec93d
Bara Konateh <bkonateh@gmail.com>
2021-04-24 22:25:16 +0100

    Revert "Revert "Added particles effect and text animation""
    This reverts commit 561a99d98a3b74e9882312ab3d558470ac9059c2.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 const Header = () => {
     return (
         <header className = 'header'>
-            <h1> React.js with Three.js</h1>
+            <h1 data-text = "Bara's Canvas"> Bara's Canvas</h1>
         </header>
+        
     )
 }
 
