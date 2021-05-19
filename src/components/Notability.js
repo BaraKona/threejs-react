741cdd4dbb28aeb63f45c1f8796e0c4eecf14a34
Bara Konateh <bkonateh@gmail.com>
2021-05-19 23:06:29 +0100

    Journality page (adding vanilla tilt)

--- src/components/Notability.js
+++ src/components/Notability.js
@@ -1,9 +1,24 @@
+import Journality from '../img/Journality.png';
+import JournalitySc from '../img/JournalitySc.png';
+import VanillaTilt from 'vanilla-tilt';
+
 const Notability = () => {
     return (
-        <body className = 'notability'>
-            <script> </script>
-            <h1> Notability </h1>
+        <body className = 'Notability'>
+
+            <h1> Journality </h1>
             <h2> The Productivity Tool *</h2>
+            
+            <body1>
+                Notability is a concept brought to life during the summer of 2019 before my first year of university.
+                <br></br><br></br>I wanted to gain some coding experience before I started my course, and having had no
+                previous experience in computer science other than modding games and *torrenting software, it was a great place to start.   
+            </body1>
+            <body2>
+            Using visual studio,  stack overflow and an impossible number of youtube videos, I brought my project to life.
+            <br></br><br></br>The concept was to make an application to boost my productivity. 
+            </body2>
+            <img src = {Journality} alt = "img of journality main interface"></img>
             <pre className = 'code'>
                 <code>
                     
